0c64c1d3ae4661756cdad81974f48fb6381e2098
Joseph LaFreniere <git@lafreniere.xyz>
2026-09-16 19:08:07 -0500

    ci: add Nix builds against Emacs 29 and 30

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
+    nixpkgs-24_05.url = "github:NixOS/nixpkgs/nixos-24.05"; # Emacs 29
+    nixpkgs-24_11.url = "github:NixOS/nixpkgs/nixos-24.11"; # Emacs 30
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -36,14 +38,77 @@
           self',
           inputs',
           pkgs,
+          system,
           ...
         }:
+        let
+          # Use emacs-overlay's MELPA/ELPA recipe archives without pulling in
+          # its extra Emacs builds.  This lets us build against bleeding edge
+          # Emacs package dependencies.
+          withMelpaOverlay =
+            nixpkgsFlake:
+            import nixpkgsFlake {
+              inherit system;
+              overlays = [ inputs.emacs-overlay.overlays.package ];
+            };
+
+          pkgsEmacs29 = withMelpaOverlay inputs.nixpkgs-24_05;
+          pkgsEmacs30 = withMelpaOverlay inputs.nixpkgs-24_11;
+          pkgsEmacs31 = withMelpaOverlay inputs.nixpkgs;
+
+          emacsPackages29 = pkgsEmacs29.emacsPackagesFor (
+            # The nixos-24.05 cache ships a native-comp Emacs 29 build whose
+            # linked libgccjit crashes dyld on current macOS; disabling
+            # native-comp forces a local build against this host's toolchain
+            # instead of using that broken cached binary.
+            pkgsEmacs29.emacs.override { withNativeCompilation = !pkgs.stdenv.isDarwin; }
+          );
+          emacsPackages30 = pkgsEmacs30.emacsPackagesFor pkgsEmacs30.emacs;
+          emacsPackages31 = pkgsEmacs31.emacsPackagesFor pkgsEmacs31.emacs;
+
+          counselProjectileFor =
+            emacsPackages:
+            emacsPackages.melpaBuild {
+              pname = "counsel-projectile";
+              version = "0.0.0";
+              commit = "unknown";
+              src = ./.;
+              recipe = pkgs.writeText "counsel-projectile-recipe" ''
+                (counsel-projectile :fetcher git :url "" :files ("counsel-projectile.el"))
+              '';
+              packageRequires = with emacsPackages; [
+                counsel
+                projectile
+              ];
+            };
+
+          loadTest =
+            name: emacsPackages: package:
+            pkgs.runCommand "counsel-projectile-load-${name}" { } ''
+              ${
+                emacsPackages.emacsWithPackages (epkgs: [ package ])
+              }/bin/emacs --batch --eval "(require 'counsel-projectile)"
+              touch $out
+            '';
+        in
         {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
           packages = {
-            default = pkgs.hello;
+            counsel-projectile-emacs-29 = counselProjectileFor emacsPackages29;
+            counsel-projectile-emacs-30 = counselProjectileFor emacsPackages30;
+            counsel-projectile-emacs-31 = counselProjectileFor emacsPackages31;
+            default = self'.packages.counsel-projectile-emacs-31;
+          };
+
+          checks = {
+            counsel-projectile-load-emacs-29 =
+              loadTest "emacs-29" emacsPackages29
+                self'.packages.counsel-projectile-emacs-29;
+            counsel-projectile-load-emacs-30 =
+              loadTest "emacs-30" emacsPackages30
+                self'.packages.counsel-projectile-emacs-30;
+            counsel-projectile-load-emacs-31 =
+              loadTest "emacs-31" emacsPackages31
+                self'.packages.counsel-projectile-emacs-31;
           };
 
           pre-commit = {
@@ -71,7 +136,6 @@
           };
 
           devShells.default = pkgs.mkShell {
-            # Inherit all of the pre-commit hooks.
             inputsFrom = [
               config.pre-commit.devShell
               config.treefmt.build.devShell
